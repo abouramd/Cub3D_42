19e3a412fe7b1f1a7f6c47c07fe67022ea35ba3f
Zakaria Sabri <zasabri@e2r8p5.1337.ma>
2023-08-05 12:26:33 +0100

    @5tirner

--- ray_casting/ft_move.c
+++ ray_casting/ft_move.c
@@ -1,6 +1,6 @@
 #include "../include/ray_casting.h"
 
-bool ft_check_hit(char c)
+bool	hit(char c)
 {
 	if (c == '1')
 		return (true);
@@ -19,17 +19,17 @@ void	move_player(t_global *data, double ang, double *x, double *y)
 	(*x) += co * 3;
 	(*y) += si * 3;
 	sx = -1;
- 	while (++sx < P_STEP)
+	while (++sx < P_STEP)
 	{
-		if (ft_check_hit(data->map[(int)(*y) / CUB_SIZE][(int)(*x) / CUB_SIZE]))
-			break;
+		if (hit(data->map[(int)(*y) / CUB_SIZE][(int)(*x) / CUB_SIZE]))
+			break ;
 		(*x) += co;
 	}
 	sy = -1;
 	while (++sy < P_STEP)
 	{
-		if (ft_check_hit(data->map[(int)(*y) / CUB_SIZE][(int)data->player_x / CUB_SIZE]))
-			break;
+		if (hit(data->map[(int)(*y) / CUB_SIZE][(int)data->player_x / CUB_SIZE]))
+			break ;
 		(*y) += si;
 	}
 	(*x) -= co * 3;
@@ -38,13 +38,13 @@ void	move_player(t_global *data, double ang, double *x, double *y)
 
 void	ft_move(t_global *data, double ang)
 {
-	double x;
-	double y;
+	double	x;
+	double	y;
 
 	x = data->player_x;
 	y = data->player_y;
 	move_player(data, ang, &x, &y);
-	if (!ft_check_hit(data->map[(int)y / CUB_SIZE][(int)data->player_x / CUB_SIZE]))
+	if (!hit(data->map[(int)y / CUB_SIZE][(int)data->player_x / CUB_SIZE]))
 	{
 		data->player_x = x;
 		data->player_y = y;
--- ray_casting/map_2d.c
+++ ray_casting/map_2d.c
@@ -1,52 +1,54 @@
 #include "../include/ray_casting.h"
 
-void ft_draw_player(t_data *img, size_t xc, size_t yc, int radius) {
-  int err;
-  int color;
-  int x;
-  int y;
+void	ft_draw_player(t_data *img, size_t xc, size_t yc, int radius)
+{
+	int	err;
+	int	color;
+	int	x;
+	int	y;
 
-  if (radius < 0)
-    return;
-  ft_draw_player(img, xc, yc, radius - 1);
-  color = WHITE;
-  x = -radius;
-  y = 0;
-  err = 2 - 2 * radius;
-  while (x < 0) {
-    my_mlx_pixel_put(img, xc - x, yc + y, color);
-    my_mlx_pixel_put(img, xc - y, yc - x, color);
-    my_mlx_pixel_put(img, xc + x, yc - y, color);
-    my_mlx_pixel_put(img, xc + y, yc + x, color);
-    radius = err;
-    if (radius <= y)
-      err += ++y * 2 + 1;
-    if (radius > x || err > y)
-      err += ++x * 2 + 1;
-  }
+	if (radius < 0)
+		return ;
+	ft_draw_player(img, xc, yc, radius - 1);
+	color = WHITE;
+	x = -radius;
+	y = 0;
+	err = 2 - 2 * radius;
+	while (x < 0)
+	{
+		my_mlx_pixel_put(img, xc - x, yc + y, color);
+		my_mlx_pixel_put(img, xc - y, yc - x, color);
+		my_mlx_pixel_put(img, xc + x, yc - y, color);
+		my_mlx_pixel_put(img, xc + y, yc + x, color);
+		radius = err;
+		if (radius <= y)
+			err += ++y * 2 + 1;
+		if (radius > x || err > y)
+			err += ++x * 2 + 1;
+	}
 }
 
-void    map_2d(t_global *data)
+void	map_2d(t_global *data)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    j = 0;
-    while (j < data->hiegth * CUB_SIZE)
-    {
-        i = 0;
-        while (i < data->width * CUB_SIZE)
-        {
-            if (j % CUB_SIZE == 0 || i % CUB_SIZE == 0)
-                my_mlx_pixel_put(&data->img, i, j, WHITE);
-            else if (data->map[j / CUB_SIZE][i / CUB_SIZE] == '1')
-                my_mlx_pixel_put(&data->img, i, j, RED);
-            else
-                my_mlx_pixel_put(&data->img, i, j, BLACK);
-            i++;
-        }
-        j++;
-    }
-    ft_draw_player(&data->img, data->player_x, data->player_y, 5);
+	i = 0;
+	j = 0;
+	while (j < data->hiegth * CUB_SIZE)
+	{
+		i = 0;
+		while (i < data->width * CUB_SIZE)
+		{
+			if (j % CUB_SIZE == 0 || i % CUB_SIZE == 0)
+				my_mlx_pixel_put(&data->img, i, j, WHITE);
+			else if (data->map[j / CUB_SIZE][i / CUB_SIZE] == '1')
+				my_mlx_pixel_put(&data->img, i, j, RED);
+			else
+				my_mlx_pixel_put(&data->img, i, j, BLACK);
+			i++;
+		}
+		j++;
+	}
+	ft_draw_player(&data->img, data->player_x, data->player_y, 5);
 }
