e35ac3e05f905f842207c2b5ac76735b6b8a85f4
Abdelhay Bouramdane <abouramd@e2r13p11.1337.ma>
2023-08-08 16:26:49 +0100

    @abouramd

--- bouns/pars/check_map_bouns.c
+++ bouns/pars/check_map_bouns.c
@@ -7,7 +7,7 @@ int	map_check(t_pars *ptr, size_t x, size_t y, int *np)
 
 	if (ptr->map[y][x] == ' ' || y == 0 || x == 0 || !ptr->map[y + 1] || !ptr->map[y][x + 1])
 		return (put_err("the map should be sorounding by wall", ptr->map[y]), -1);
-	if (!(ptr->map[y][x] == '0' || ptr->map[y][x] == 'N' || ptr->map[y][x] == 'S' || ptr->map[y][x] == 'E' || ptr->map[y][x] == 'W'))
+	if (!(ptr->map[y][x] == '0' || ptr->map[y][x] == 'N' || ptr->map[y][x] == 'S' || ptr->map[y][x] == 'E' || ptr->map[y][x] == 'W' || ptr->map[y][x] == 'D'))
 				return (put_err("Unknowing char", ptr->map[y]), -1);
 	if ((ptr->map[y][x] == 'N' || ptr->map[y][x] == 'S' || ptr->map[y][x] == 'E' || ptr->map[y][x] == 'W') && !(*np)++)
 	{
@@ -43,7 +43,7 @@ int	run_check(t_pars *ptr)
 		y = 0;
 		while (ptr->map[x][y])
 		{
-			if ((ptr->map[x][y] == '0' || ptr->map[x][y] == 'E' || ptr->map[x][y] == 'N' || ptr->map[x][y] == 'S' || ptr->map[x][y] == 'W')
+			if ((ptr->map[x][y] == '0' || ptr->map[x][y] == 'D' || ptr->map[x][y] == 'E' || ptr->map[x][y] == 'N' || ptr->map[x][y] == 'S' || ptr->map[x][y] == 'W')
 				&& map_check(ptr, y, x, &np) == -1)
 				return (-1);
 			else if (ptr->map[x][y] != '1' && ptr->map[x][y] != ' ')
